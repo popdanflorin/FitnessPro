65899d60a07b7a2c891e5c21fd142588d3c72a1e
florinp <florin.pop@tecknoworks.com>
2017-08-28 12:49:38 +0300

    intial commit of the project

---
+++ FitnessPro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitnessPro.Startup))]
+namespace FitnessPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
