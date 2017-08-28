a1899c2c07ec78af72cf7191122bf389f1d6f415
florinp <florin.pop@tecknoworks.com>
2017-08-28 13:39:42 +0300

    added knockout.js to solution

--- FitnessPro/App_Start/BundleConfig.cs
+++ FitnessPro/App_Start/BundleConfig.cs
@@ -22,6 +22,11 @@ namespace FitnessPro
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                       "~/Scripts/knockout-3.4.2.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/viewModels").Include(
+                      "~/Scripts/ViewModels/*.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
