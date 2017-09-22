0bf0b04dc0035974d58d64010b45e58a4fbd1ec4
apolka <apolka.szasz@tecknoworks.com>
2017-09-22 12:31:16 +0300

    comentarii - corectat

--- FitnessPro/Migrations/Configuration.cs
+++ FitnessPro/Migrations/Configuration.cs
@@ -28,7 +28,7 @@ namespace FitnessPro.Migrations
               new Workout { Id = Guid.NewGuid().ToString(), Name = "The pullup-pushup workout routine", Description = "Lock down a great upper body with the only two exercises you really needs.", Type = WorkoutType.Chest, Active = true }
               
             );
-            context.WorkoutExercises.AddOrUpdate(
+            /*context.WorkoutExercises.AddOrUpdate(
                 p => p.Name,
                 //new WorkoutExercise { Id = Guid.NewGuid().ToString(), Name = "Ab Crunch", Description = "Begins with lying face up on the floor with knees bent. The movement begins by curling the shoulders towards the pelvis. The hands can be behind or beside the neck or crossed over the chest.", WorkoutId = context.Workouts.Where(w => w.Name ==  "Six-pack in six weeks").Id, ActiveEx = true, Repetitions = 50 },
                 //trebuie luata Id-ul workoutului cumva unde numele este "Six..."
@@ -36,7 +36,7 @@ namespace FitnessPro.Migrations
                 //new WorkoutExercise { Id = Guid.NewGuid().ToString(), Name = "Knee Raise ", Description = "Start by lying on your back with your arms next to your sides and your legs extended. Tighten your abdominals and raise your feet and legs about 3 inches off the floor to come into the starting position. Bend your knees and pull them into your chest as far as you can. Reverse the motion and return your legs to the starting position.", WorkoutId = "617b8405-e2bf-4ae2-81f4-4a03b9ba97e5", ActiveEx = true, Repetitions = 30 },
                 //new WorkoutExercise { Id = Guid.NewGuid().ToString(), Name = "Sit Up", Description = "Lay down with your knees bent and your feet placed flat on the ground. Cross your arms over your chest so your hands touch the opposite shoulders. Tighten your abdominal muscles. Draw your belly button in to your spine. Slowly sit up. Bring your head up first, then your shoulders. Keep your feet on the ground. Hold the position for a second. Slowly lay back down.", WorkoutId = "617b8405-e2bf-4ae2-81f4-4a03b9ba97e5", ActiveEx = true, Repetitions = 50 }
                 
-                );
+                );*/
         }
     }
 }
