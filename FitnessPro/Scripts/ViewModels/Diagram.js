ff59965e4ad6c31241cc26f0ed45efdcd47edffc
apolka <apolka.szasz@tecknoworks.com>
2017-09-27 11:14:45 +0300

    Diagram

--- FitnessPro/Scripts/ViewModels/Diagram.js
+++ FitnessPro/Scripts/ViewModels/Diagram.js
@@ -11,6 +11,26 @@
             success: function (data) {
                 console.log(data);
                 self.datePercentageList(data.datePercentageList);
+                var percentageList = [];
+                var dateList = [];
+                var len = self.datePercentageList().length;
+                for (var i = 0, len; i < len; i++) {
+                    percentageList[i] = self.datePercentageList()[i].Percentage;
+                    dateList[i] = self.datePercentageList()[i].Date;
+                   
+                }
+                var ctx = document.getElementById("resultChart");
+                var resultChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: dateList,
+                        datasets: [
+                          {
+                              data: percentageList
+                          }
+                        ]
+                    }
+                });
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(textStatus + ': ' + errorThrown);
@@ -18,26 +38,8 @@
         });
         
     }
-    var percentageList = [];
-    var dateList = [];
-    var len = self.datePercentageList().length;
-    for (var i = 0, len; i < len; i++) {
-        percentageList[i] = data.datePercentageList()[i].Percentage;
-        dateList[i] = data.datePercentageList()[i].Date;
-    }
-    var ctx = document.getElementById("resultChart");
-    var resultChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: dateList,
-            datasets: [
-              {
-                  data: percentageList
-              }
-            ]
-        }
-    });
-   var date = ["2017-09-11", "2017-10-01", "2017-10-03", "2017-08-10"];
+    
+   /*var date = ["2017-09-11", "2017-10-01", "2017-10-03", "2017-08-10"];
    var percentage = [107.00, 96.22, 203.22, 50.00];
     var ctx = document.getElementById("resultChart");
     var resultChart = new Chart(ctx, {
@@ -50,5 +52,5 @@
               }
             ]
         }
-    })
+    })*/
 }
